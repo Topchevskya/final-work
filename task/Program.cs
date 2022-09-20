d5e8dcaadd50164c674773601cd0eebf1c0a123f
Elena <Dafinia@mail.ru>
2022-09-20 21:53:04 +0300

    task

---
+++ task/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+string[] array = { "hello", "2", "world", ";-)" };
+string[] newArray = new string[array.Length];
+void FullArray(string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+FullArray(newArray);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(newArray);
